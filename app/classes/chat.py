a5be769b0d380c087908d16a2596347b357f4c6a
Leonid Savchenkov <smog@list.ru>
2017-09-20 00:20:48 +0300

    initial commit

---
+++ app/classes/chat.py
@@ -0,0 +1,12 @@
+from ..settings import bot
+
+class Chat(object):
+
+    def __init__(self, chat_id, text=''):
+        self.chat_id = chat_id
+        self.text = text
+
+    # main process method
+    def process(self):
+        bot.sendMessage(chat_id=self.chat_id, text=u"> {}".format(self.text))
+		
