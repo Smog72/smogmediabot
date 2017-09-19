0f473ed5834fe7bbdf23bea8fca2ab3432aa2aab
Leonid Savchenkov <smog@list.ru>
2017-09-20 00:52:45 +0300

    2nd commit - ready for deploy?

--- app/classes/settings.py
+++ app/classes/settings.py
@@ -1,4 +1,4 @@
 import telegram
 BOT_TOKEN = '439018784:AAH59uW_TfmQYugqpkqyDVi9Yu0DcxKbYXc'
-WEBHOOK_URL = 'your_dokkur_url/HOOK'
+WEBHOOK_URL = 'smogmediabot-master.dokkurapp.com/HOOK'
 bot = telegram.Bot(token=BOT_TOKEN)
